45748688147738cc2a491e5c7136fc3aabf7026b
Viacheslav Popov <qustaine@gmail.com>
2022-04-03 10:12:19 +0100

    New animation system

--- Player/Player.gd
+++ Player/Player.gd
@@ -10,7 +10,11 @@ export var slowdown_coefficient_air = 0.05
 
 var velocity = Vector2.ZERO
 
-onready var animatedSprite = $AnimatedSprite
+onready var sprite = $Sprite
+onready var animationTree = $AnimationTree
+
+func _ready():
+	animationTree.active = true
 
 func _physics_process(delta):
 	velocity.y += gravity * delta
@@ -18,38 +22,39 @@ func _physics_process(delta):
 	var apply_friction = false
 	
 	if Input.is_action_pressed("ui_right"):
+		animationTree.set("parameters/Movement/current", 1)
 		if velocity.x < 0:
 			velocity.x *= (1 - slowdown_coefficient_ground)   #helps to change direction faster
 		velocity.x = min(velocity.x + acceleration * delta, max_speed)
-		animatedSprite.flip_h = false
-		if is_on_floor():
-			animatedSprite.play("Run")
+		sprite.flip_h = false
 	elif Input.is_action_pressed("ui_left"):
+		animationTree.set("parameters/Movement/current", 1)
 		if velocity.x > 0:
 			velocity.x *= (1 - slowdown_coefficient_ground)
 		velocity.x = max(velocity.x - acceleration * delta, -max_speed)
-		animatedSprite.flip_h = true
-		if is_on_floor():
-			animatedSprite.play("Run")
+		sprite.flip_h = true
 	else:
+		animationTree.set("parameters/Movement/current", 0)
 		apply_friction = true
-		if is_on_floor():
-			animatedSprite.play("Idle")
 	
 	if is_on_floor():
+		animationTree.set("parameters/InAirState/current", 0)
 		if Input.is_action_just_pressed("jump"):
 			velocity.y -= jump_force
+			animationTree.set("parameters/InAir/current", 1)
+			animationTree.set("parameters/InAirState/current", 1)
 		if apply_friction:
 			velocity.x = lerp(velocity.x, 0, slowdown_coefficient_ground)
-	elif Input.is_action_just_released("jump") and velocity.y < 0:
-		velocity.y /= jump_decrease_on_release_coefficient
+	else:
+		if velocity.y > 0:
+			animationTree.set("parameters/InAir/current", 0)
+		animationTree.set("parameters/InAirState/current", 1)
+		if Input.is_action_just_released("jump"):
+			animationTree.set("parameters/InAir/current", 0)
+			if velocity.y < 0:
+				velocity.y /= jump_decrease_on_release_coefficient
 		
-	if not is_on_floor():
-		animatedSprite.play("Jump")
-		if apply_friction:
-			velocity.x = lerp(velocity.x, 0, slowdown_coefficient_air)
+	if not is_on_floor() and apply_friction:
+		velocity.x = lerp(velocity.x, 0, slowdown_coefficient_air)
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
-	
-	if Input.is_action_just_pressed("attack"):
-		animatedSprite.play("Attack")
